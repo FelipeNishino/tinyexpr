42ed0f53a940ea9abafbe181c10842e052c731b2
FelipeNishino <felipenishino@gmail.com>
2022-06-13 20:28:52 -0300

    Changed example to text with power

--- examplefac.c
+++ examplefac.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     double r;
-    const char *expression = "5! + (3 * 2)! - 4!";
+    const char *expression = "5! + (3 ^ 2)! - 4!";
     r = te_interp(expression, 0);
     printf("The expression:\n\t%s\nevaluates to:\n\t%f\n", expression, r);
     return 0;
