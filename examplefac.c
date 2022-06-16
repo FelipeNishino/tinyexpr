86dfe91a26962044d7ae7925d574bdfb4d110100
FelipeNishino <felipenishino@gmail.com>
2022-06-16 14:31:07 -0300

    Added example "2 * 5! / 3" to examplefac

--- examplefac.c
+++ examplefac.c
@@ -6,5 +6,10 @@ int main(int argc, char *argv[]) {
     const char *expression = "5! + (3 ^ 2)! - 4!";
     r = te_interp(expression, 0);
     printf("The expression:\n\t%s\nevaluates to:\n\t%f\n", expression, r);
+
+    const char *expression2 = "2 * 5! / 3";
+    r = te_interp(expression2, 0);
+    printf("The expression:\n\t%s\nevaluates to:\n\t%f\n", expression2, r);
+
     return 0;
 }
